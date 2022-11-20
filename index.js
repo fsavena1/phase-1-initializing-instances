21d8bce60b219dfb93450d7e75f16016c43b586c
frank <franksavena@gmail.com>
2022-11-20 11:47:57 -0600

    commit tree d2dda24567ba27a45937244382699be6f4f0f707

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your code here
+
+class Breakfast {
+    constructor(food, drink){
+        this.food = food;
+        this.drink = drink;
+    }
+}
+
+class Lunch {
+    constructor(salad, soup, drink) {
+        this.soup = soup 
+        this.salad = salad
+        this.drink = drink
+    }
+}
+const newLunch = new Lunch('side salad', 'broccoli cheddar soup', 'iced tea')
+
+class Dinner {
+    #dessert
+    constructor(salad, soup, entree, dessert){
+        this.soup = soup
+        this.salad = salad
+        this.entree = entree
+        this.#dessert = dessert
+    }
+ }
+
+ const newDinner = new Dinner ('consomme' , 'balsamic salad', 'filet mignon', 'cheesecake')
